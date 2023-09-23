396c9b177635da2d0f963af5117afdc70200d7f4
Hajra Batool <hamna64gb@gmail.com>
2023-09-23 22:50:14 +0500

    Add files via upload

---
+++ program_8.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a=10,b=15;
+  int temp;
+  cout<<" Before swap "<<a<<" "<<b<<endl;
+  temp=a;
+  a=b;
+  b=temp;
+  cout<<" After swap "<<a<<" "<<b<<endl;
+       
+}
